5670d47d81d64c0a1995360e27b799eab0f65ab4
ajslll <113236359+ajslll@users.noreply.github.com>
2022-11-05 19:17:52 +0100

    add

---
+++ main.py
@@ -0,0 +1,34 @@
+#1
+'''def change(lst):
+    if len(lst) < 2:
+        print('Error')
+        exit()
+    else:
+        lst[0], lst[-1] = lst[-1], lst[0]
+    return lst
+print(change(['Candle', 'shop', 123, 'Mac', 12]))'''
+
+#2
+'''def to_dict(lst):
+    return {element: element for element in lst}
+print(to_dict([123,'Mac', 'Candle', (321, 12)]))'''
+
+#3
+'''def sum_range(start, end):
+    if start > end:
+        end, start = start, end
+    return sum(range(start, end + 1))
+print(sum_range(1, 5))'''
+
+#4
+
+def read_last(lines, file):
+    if lines < 1 or type(lines) != int:
+        print('Кількість строк може бути лише цілим, додатнім числом')
+    else:
+        with open(file, encoding='utf-8') as k:
+            file_lines = k.readlines()[-lines:]
+        for j in file_lines:
+            print(j.strip())
+
+read_last(3, 'file.txt')
